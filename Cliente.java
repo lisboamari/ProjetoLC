fa38913bd194fccb6671f3f861b6cf80bb83799e
lisboamari <74574458+lisboamari@users.noreply.github.com>
2022-07-26 15:03:31 -0300

    Criação da classe Cliente

---
+++ Cliente.java
@@ -0,0 +1,36 @@
+public class Cliente {
+
+    private String nomeCliente;
+    private String tipoCliente;
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public Cliente () {
+
+    }
+    
+    public Cliente (String nomeCliente, String tipoCliente) {
+        this.nomeCliente = nomeCliente;
+        this.tipoCliente = tipoCliente;
+    }
+    
+    @Override
+    public String toString() {
+        return "Dados do Cliente: " +
+                "Nome do Cliente: " + nomeCliente + 
+                ", Tipo de Cliente: " + tipoCliente;
+
+    }
+    
+}

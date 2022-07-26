b2d5843ebd9eb1dc95bb3199cf122f28074e3a8d
mario-watanabe <109993307+mario-watanabe@users.noreply.github.com>
2022-07-26 18:36:13 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -46,4 +46,22 @@ public class Conta {
                 ", Nome do Cliente: " + titular;
     }
     
+    private final List<Conta> contass = new ArrayList<>();
+ 
+    // Construtor da classe Conta populando o array contas
+    // instanciando objetos da classe Conta
+        this.contas.add(new Conta(
+            1, "Guimarães"));
+        this.contas.add(new Conta(
+            2, "Mariana"));
+        this.contas.add(new Conta(
+            3, "Watanabe"));
+        this.contas.add(new Conta(
+            101, "Empresa X"));
+        this.contas.add(new Conta(
+            102, "Empresa Y"));
+        this.contas.add(new Conta(
+            103, "Empreza Z"));
+   
+    
 }

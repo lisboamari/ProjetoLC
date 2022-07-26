0fbabcf42cf2c94de46c0fb3bf79f3c6ffac68c6
mario-watanabe <109993307+mario-watanabe@users.noreply.github.com>
2022-07-26 17:33:29 -0300

    Create ClientePF.java

---
+++ ClientePF.java
@@ -0,0 +1,15 @@
+public class ClientePF extends Cliente {  
+
+        
+    public ClientePF() {}
+
+    public ClientePF(String nome, String tipoCliente) {
+        super(nome, tipoCliente);
+        
+    }
+    
+
+    
+
+    
+}

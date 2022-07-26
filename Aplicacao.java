1b3af5dc0e9955bd4e88bb175251138f80e5e468
mario-watanabe <109993307+mario-watanabe@users.noreply.github.com>
2022-07-26 17:11:14 -0300

    Create Aplicacao.java

---
+++ Aplicacao.java
@@ -0,0 +1,25 @@
+public class Aplicacao {
+   // testa aplicacao Banco Lets Code
+    public static void main(String[] args) throws Exception {
+     Aplicacao aplicacao = new Aplicacao();
+     aplicacao.abrirConta();
+     aplicacao.sacar();
+     aplicacao.depositar();
+     aplicacao.transferencia();
+     aplicacao.investir();
+     aplicacao.consultarSaldo();
+     
+   } 
+     private void abrirConta() {}
+
+     private void sacar() {};
+ 
+     private void depositar() {};
+
+     private void transferencia() {};
+
+     private void investir() {};
+
+     private void consultarSaldo() {};
+
+}

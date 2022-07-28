dc56399e786803d9f96fe3b821d1545ada88ad94
mario-watanabe <109993307+mario-watanabe@users.noreply.github.com>
2022-07-28 10:10:08 -0300

    Update Aplicacao.java

--- Aplicacao.java
+++ Aplicacao.java
@@ -1,13 +1,34 @@
 public class Aplicacao {
    // testa aplicacao Banco Lets Code
     public static void main(String[] args) throws Exception {
-     Aplicacao aplicacao = new Aplicacao();
-     aplicacao.abrirConta();
-     aplicacao.sacar();
-     aplicacao.depositar();
-     aplicacao.transferencia();
-     aplicacao.investir();
-     aplicacao.consultarSaldo();
+        private final List<Conta> contasCorrentes = new ArrayList<>();
+ 
+     // Construtor da classe ContaCorrente populando o array contasCorrentes
+     // instanciando objetos da classe ContaCorrente
+        this.contasCorrenes.add(new ContaCorrente(
+            1, "Guimarães"));
+        this.contasCorrentes.add(new ContaCorrente(
+            2, "Mariana"));
+        this.contasCorrentes.add(new ContaCorrente(
+            3, "Watanabe"));
+        this.contasCorrentes.add(new ContaCorrente(
+            101, "Empresa X"));
+        this.contasCorrentes.add(new ContaCorrente(
+            102, "Empresa Y"));
+        this.contasCorrentes.add(new ContaCorrente(
+            103, "Empreza Z"));     
+    
+       
+     ContaCorrente conta = new ContaCorrente();
+     conta.abrirConta();
+     conta.sacar(valor);
+     conta.depositar(valor);
+     conta.transferencia(valor, contaDestino);
+     conta.investirContaPoupanca(valor);
+     conta.investirContaInvestiemento(valor);  
+     conta.consultarSaldoContaCorrente();
+     conta.consultarSaldoContaPoupanca();
+     conta.consultarSaldoContaInvestimento();
      
    } 
      private void abrirConta() {}

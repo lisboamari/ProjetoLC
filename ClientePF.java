6d6a40c9e1e10f9aeffbaeb86ca7749dd592421c
lisboamari <74574458+lisboamari@users.noreply.github.com>
2022-07-28 17:39:36 -0300

    Adicionando Contas possíveis de serem utilizadas

--- ClientePF.java
+++ ClientePF.java
@@ -1,15 +1,44 @@
 public class ClientePF extends Cliente {  
 
-        
+    private ContaCorrente CC = new ContaCorrente();
+    private ContaPoupanca CP = new ContaPoupanca();
+    private ContaInvestimento CI = new ContaInvestimento();
+
     public ClientePF() {}
 
-    public ClientePF(String nome, String identificadorCliente) {
+    public ClientePF(String nome, String identificadorCliente, ContaCorrente CC) {
         super(nome, identificadorCliente);
+        this.CC = CC;
         
     }
-    
 
-    
+    public ClientePF(String nome, String identificadorCliente, ContaPoupanca CP) {
+        super(nome, identificadorCliente);
+        this.CP = CP;
+        
+    }
 
+    public ClientePF(String nome, String identificadorCliente, ContaInvestimento CI) {
+        super(nome, identificadorCliente);
+        this.CI = CI;
+        
+    }
+
+    public ContaCorrente getContaCorrente() {
+        return CC;
+    }
+
+    public ContaPoupanca getContaPoupanca() {
+        return CP;
+    }
+
+    public ContaInvestimento gContaInvestimento() {
+        return CI;
+    }
+
+    @Override
+    public String toString() {
+        return "Identificador da Conta: " + identificadorCliente + "Nome:"+ nomeCliente;
+    }
     
 }

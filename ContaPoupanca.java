02433ee1b7a732bce77a1ce8223d6f28fb58d193
xguimaraesx <32361580+xguimaraesx@users.noreply.github.com>
2022-07-28 17:36:06 -0300

    método acidente de juros.

--- ContaPoupanca.java
+++ ContaPoupanca.java
@@ -7,9 +7,10 @@ public class ContaPoupanca extends Conta {
         super(numeroConta, titular);
         
     }
-    
 
-    
-
+    public void gerarAcidente() {
+        saldo=saldo*0.005;
+        System.out.println("Juros adicionados");
+      }
     
 }

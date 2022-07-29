2d7327ba475a53b5e8751377aa8d90ad6c2eb306
xguimaraesx <32361580+xguimaraesx@users.noreply.github.com>
2022-07-29 14:28:28 -0300

    Escopo reduzido da aplicação

--- Aplicacao.java
+++ Aplicacao.java
@@ -1,68 +1,37 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Aplicacao {
-   // testa aplicacao Banco Lets Code
+
     public static void main(String[] args) throws Exception {
-     
-       private final List<ClientePF> clientesPF = new ArrayList<>();
-       private final List<ClientePJ> clientesPJ = new ArrayList<>();
- 
-    // Construtor da classe Cliente populando o array clientesPF e clientesPJ
-    // instanciando objetos das classes ClientePF e ClientePJ
-        this.clientesPF.add(new ClientePF(
-            "Guimarães","PF"));
-        this.clientesPF.add(new ClientePF(
-            "Mariana","PF"));
-        this.clientesPF.add(new ClientePF(
-            "Watanabe","PF"));
-        this.clientesPJ.add(new ClientePJ(
-            "Empresa X","PJ"));
-        this.clientesPJ.add(new ClientePJ(
-            "Empresa Y","PJ"));
-        this.clientesPJ.add(new ClientePJ(
-            "Empreza Z","PJ"));         
-   
-       private final List<Conta> contasCorrentes = new ArrayList<>();
-    
-    // Construtor da classe ContaCorrente populando o array contasCorrentes
-    // instanciando objetos da classe ContaCorrente
-        this.contasCorrenes.add(new ContaCorrente(
-            1, "Guimarães"));
-        this.contasCorrentes.add(new ContaCorrente(
-            2, "Mariana"));
-        this.contasCorrentes.add(new ContaCorrente(
-            3, "Watanabe"));
-        this.contasCorrentes.add(new ContaCorrente(
-            101, "Empresa X"));
-        this.contasCorrentes.add(new ContaCorrente(
-            102, "Empresa Y"));
-        this.contasCorrentes.add(new ContaCorrente(
-            103, "Empreza Z"));     
-    
-       
-     ContaCorrente conta = new ContaCorrente();
-     conta.abrirConta();
-     conta.sacar(valor);
-     conta.depositar(valor);
-     conta.transferencia(valor, contaDestino);
-     conta.investirContaPoupanca(valor);
-     conta.investirContaInvestiemento(valor);  
-     conta.consultarSaldoContaCorrente();
-     conta.consultarSaldoContaPoupanca();
-     conta.consultarSaldoContaInvestimento();
-     
-   } 
-     private void abrirConta() {}
 
-     private void sacar() {};
- 
-     private void depositar() {};
+        Cliente cliente = new ClientePF();
+        cliente.setIdentificadorCliente("1");
+        cliente.setNomeCliente("Guima");
 
-     private void transferencia() {};
+        Cliente cliente2 = new ClientePJ();
 
-     private void investir() {};
+        cliente2.setIdentificadorCliente("2");
+        cliente2.setNomeCliente("WataEnterprise");
 
-     private void consultarSaldo() {};
+        Conta conta = new ContaCorrente(1, cliente);
+        Conta conta2 = new ContaCorrente(2, cliente2);
 
+        conta.depositar(2000);
+        System.out.println(conta.getSaldo());
+
+        conta2.depositar(20000);
+        System.out.println(conta2.getSaldo());
+
+        conta.sacar(500);
+        System.out.println(conta.getSaldo());
+
+        conta.transferir(conta2, 500);
+        System.out.println(conta.getSaldo());
+        System.out.println(conta2.getSaldo());
+
+        conta.gerarAcidente();
+        System.out.println(conta.getSaldo());
+
+        conta2.transferir(conta, 500);
+        System.out.println(conta.getSaldo());
+        System.out.println(conta2.getSaldo());
+    }
 }

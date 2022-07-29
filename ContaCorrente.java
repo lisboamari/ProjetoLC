f4d9e5094ad7bed9f756353075abcc9198cb7c4b
xguimaraesx <32361580+xguimaraesx@users.noreply.github.com>
2022-07-29 14:35:41 -0300

    Implementação de saque diferenciado para PF e PJ

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -3,13 +3,36 @@ public class ContaCorrente extends Conta {
         
     public ContaCorrente() {}
 
-    public ContaCorrente(int numeroConta, String titular) {
-        super(numeroConta, titular);
+    public ContaCorrente(int numeroConta, Cliente cliente) {
+        super(numeroConta, cliente);
         
     }
-    
 
+    @Override
+    public void gerarAcidente() {
+        saldo=saldo-9.9;
+        System.out.println("Taxa  de admin de 9,90");
+    }
     
-
+    @Override
+    public boolean sacar(double valor) {
+        if (this.cliente instanceof ClientePF) {
+            if (valor < saldo) {
+                saldo = saldo - valor;
+                return true;
+    
+            } else {
+                return false;
+            }
+        }else {
+            if (valor < saldo) {
+                saldo = saldo - valor*1.005;
+                return true;
+    
+            } else {
+                return false;
+            }
+        }
+    }
     
 }
